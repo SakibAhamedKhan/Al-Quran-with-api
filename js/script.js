eb93ccf38a56aa7f38d15b7c12ab2dbdee3acee1
SakibAhamedKhan <sakibahamedkhan01@gmail.com>
2022-03-01 02:28:24 +0600

    surah page connect

--- js/script.js
+++ js/script.js
@@ -11,8 +11,16 @@ const displayAllPara = (data) => {
 		const div = document.createElement('div');
 		div.classList.add('para-card');
 		div.classList.add('col-3');
+		div.onclick = () => {
+			const  url = `../surah.html?surah=${d.number}`;
+			window.location.href = url;
+		}
 		div.innerHTML = `
-		<h3 class='d-flex align-items-center fs-5 fw-bold'><div class='para-number-card d-flex align-items-center justify-content-center mx-4'><div class='para-number'>${d.number}</div></div> ${d.name.transliteration.en}</h3>
+		<h3 class='d-flex align-items-center fs-5 fw-bold'><div class='para-number-card d-flex align-items-center justify-content-center'><div class='para-number'>${d.number}</div></div> ${d.name.transliteration.en}</h3>
+		<div>
+			<h3 class='fs-5 text-center'>${d.name.short}</h3>
+			<h4 class='fs-6 text-center'>Ayahs ${d.numberOfVerses}</h4>
+		</div>
 		`;
 		container.appendChild(div);
 	})
---
+++ js/surah.js
@@ -0,0 +1,5 @@
+// Get Parameter from link
+const {search} = window.location;
+const numberOfSurah = (new URLSearchParams(search)).get('surah');
+// console.log(numberOfSurah);
+
