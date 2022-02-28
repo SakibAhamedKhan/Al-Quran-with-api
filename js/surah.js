abbd174b82403fa210a64c30fc14f5f2325dc371
SakibAhamedKhan <sakibahamedkhan01@gmail.com>
2022-03-01 05:10:13 +0600

    surah page done

--- js/surah.js
+++ js/surah.js
@@ -3,3 +3,24 @@ const {search} = window.location;
 const numberOfSurah = (new URLSearchParams(search)).get('surah');
 // console.log(numberOfSurah);
 
+fetch(`https://api.quran.sutanlab.id/surah/${numberOfSurah}`)
+.then(response => response.json())
+.then(data => displaySurah(data.data));
+
+const displaySurah = (data) => {
+	console.log(data);
+	document.getElementById('surah-name').innerText = data.name.transliteration.en;
+	document.getElementById('ayah-total').innerText = `Ayah ${data.verses.length}`;
+	const container = document.getElementById('surah');
+	data.verses.forEach((d, index) => {
+		// console.log(index);
+		// const div = document.createElement('div');
+		// div.innerHTML = `
+			
+		// `;
+		container.innerHTML += `<div class='text-center fw-bold border border-2 ayah p-3 rounded my-2'>${d.text.arab} (${index+1}) <div class='d-block fw-normal pt-2'>${d.text.transliteration.en}</div> </div>`;
+
+		// <div class='d-inline fs-4'>${d.text.arab}</div> <div class='d-inline-block ayah  mx-2'>${index+1}</div>
+		// container.innerText += `${d.text.arab}`;
+	})
+}
