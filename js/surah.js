b28943dadd4ea371b25c90ccf45e9a4dfca2ccd3
SakibAhamedKhan <sakibahamedkhan01@gmail.com>
2022-03-02 13:52:12 +0600

    Play control Added

--- js/surah.js
+++ js/surah.js
@@ -1,19 +1,17 @@
 // Get Parameter from link
 const {search} = window.location;
 const numberOfSurah = (new URLSearchParams(search)).get('surah');
-// console.log(numberOfSurah);
+
 
 fetch(`https://api.quran.sutanlab.id/surah/${numberOfSurah}`)
 .then(response => response.json())
 .then(data => displaySurah(data.data, numberOfSurah));
 
 const displaySurah = (data, numberOfSurah) => {
-	console.log(data);
 	document.getElementById('surah-name').innerText = data.name.transliteration.en;
 	document.getElementById('ayah-total').innerText = `Ayah ${data.verses.length}`;
 	const container = document.getElementById('surah');
 	data.verses.forEach((d, index) => {
-		// console.log(index);
 		// const div = document.createElement('div');
 		// div.innerHTML = `
 			
@@ -29,8 +27,8 @@ const displaySurah = (data, numberOfSurah) => {
 				${str}
 			</div>	
 			<div>
-				<button class="btn  btn-success mt-3 px-4" onclick="loadAudioOfSurah(${index},${numberOfSurah},false)">Stop</button>
-				<button class="btn  btn-success mt-3 me-2 px-4" onclick="loadAudioOfSurah(${index},${numberOfSurah},true)">Play</button>
+				<button id="stopping-${index}" class="btn  btn-success mt-3 px-4" onclick="loadAudioOfSurah(${index},${numberOfSurah},false)">Stop</button>
+				<button id="playing-${index}" class="btn  btn-success mt-3 me-2 px-4" onclick="loadAudioOfSurah(${index},${numberOfSurah},true)">Play</button>
 			</div>
 		</div>
 		
@@ -40,21 +38,40 @@ const displaySurah = (data, numberOfSurah) => {
 }
 
 const loadAudioOfSurah = (index,numberOfSurah, condition) => {
-	console.log(index,numberOfSurah);
 	const url = `https://api.quran.sutanlab.id/surah/${numberOfSurah}`;
 
 	fetch(url)
 	.then(res => res.json())
-	.then(data => playAudioOfSurah(data.data.verses[index], condition));
+	.then(data => playAudioOfSurah(data.data, condition, index));
 }
 
-const playAudioOfSurah = (data, condition) => {
-	// console.log(data.audio.primary);
+const playAudioOfSurah = (data, condition , index) => {
+	const len = data.verses.length;
+	for(let i=0; i<len; i++){
+		const container = document.getElementById(`playing-${i}`);
+		container.innerText = 'Play';
+		container.classList.remove('btn-danger');
+	}
+	data = data.verses[index];
+
 	const audio = document.getElementById('audioPlay');
 	audio.setAttribute('src', data.audio.primary);
+	audio.setAttribute('onended', `playingEnded('playing-${index}')`)
 	if(condition === true){
+		const playing = document.getElementById(`playing-${index}`);
+		playing.innerText = 'Running';
+		playing.classList.add('btn-danger');
 		audio.play();
 	} else{
+		playingEnded(`playing-${index}`);
 		audio.pause();
+		
 	}
+}
+
+const playingEnded = (id) => {
+	// alert(id);
+	const playing = document.getElementById(id);
+	playing.innerText = 'Play';
+	playing.classList.remove('btn-danger');
 }
